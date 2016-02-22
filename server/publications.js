35b49ad333fe069b7c131fd3b864f77a37abd5f0
Paulo Santos <sungonku@gmail.com>
2016-02-22 11:20:50 +0000

    little fix in users publish

--- server/publications.js
+++ server/publications.js
@@ -10,5 +10,12 @@ Meteor.publish("chats", function(){
 });
 
 Meteor.publish("users", function(){
-    return Meteor.users.find();
+    if (this.userId) {
+        return Meteor.users.find({}, {
+          fields: {
+            profile: 1,
+            _id: 1
+            }
+        });
+    }
 })
